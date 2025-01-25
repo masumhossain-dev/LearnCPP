718c0190bac1a1578aef6608ee76ad05d5fb93ce
masumhossain-dev <masumhossain.dev@gmail.com>
2025-01-25 18:39:53 +0600

    while loop practicing code is successfully executed.

--- while_loop.cpp
+++ while_loop.cpp
@@ -3,6 +3,18 @@ using namespace std;
 
 int main()
 {
+int i = 1;
+    int user_need;
+    cout << "Enter your number: " << endl;
+    cin>> user_need;
+
+    while(i <= user_need)
+    {
+        cout <<"Hello Bangladesh!" << endl;
+        i++;
+    }
+
+
 
     return 0;
 }
