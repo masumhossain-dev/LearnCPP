7d32e09f92e35e00e69d695a0964bfcd42610647
masumhossain-dev <masumhossain.dev@gmail.com>
2025-01-25 18:48:44 +0600

    do while loop completed.

---
+++ do_while.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+int i = 1;
+    int user_need;
+    cout << "Enter the number of you want:" << endl;
+    cin >> user_need;
+
+    do
+    {
+        cout<< "Hello Bangladesh" << endl;
+        i++;
+    }while (i<=user_need);
+
+
+    return 0;
+}
