ce99a10652253e287b8e3e609a21f640bec5ab0d
masumhossain-dev <masumhossain.dev@gmail.com>
2025-01-26 00:55:14 +0600

    make some changes in this code

--- multiplication_table.cpp
+++ multiplication_table.cpp
@@ -7,6 +7,7 @@ int num;
     cout << "Enter your multiplied number: " << endl;
     cin >> num;
 
+    cout << "Your required multiplication table is: " << endl;
     for (int i = 1; i<=10; i++)
     {
         cout << num << " X " << i << " = " << num * i << endl;
