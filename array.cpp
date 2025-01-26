42ffcbef9034cd18d55ff22c8b0e30f15f45d7d3
masumhossain-dev <masumhossain.dev@gmail.com>
2025-01-26 18:35:22 +0600

    get user input in an array and display the array using for loop

--- array.cpp
+++ array.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 int main()
 {
+    //array initialization and display using for loop.
 int age[10] = {11, 10, 32, 25, 54, 23, 66, 76, 18, 23};
 
     for(int i = 0; i<10; i++)
@@ -10,6 +11,19 @@ int age[10] = {11, 10, 32, 25, 54, 23, 66, 76, 18, 23};
         cout << age[i] << endl;
     }
 
+
+// get input in array using for loop.
+    string name[10];
+
+    for (int i = 0; i<10; i++)
+    {
+        cin >> name[i];
+    }
+
+    for (int i = 0; i<10; i++)
+    {
+        cout << name[i] << endl;
+    }
 
     return 0;
 }
