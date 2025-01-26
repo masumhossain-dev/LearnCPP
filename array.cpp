f4c21b0248a011b927e854f100fe9e13ab6dd511
masumhossain-dev <masumhossain.dev@gmail.com>
2025-01-26 18:27:12 +0600

    age list printed using array.

---
+++ array.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+int age[10] = {11, 10, 32, 25, 54, 23, 66, 76, 18, 23};
+
+    for(int i = 0; i<10; i++)
+    {
+        cout << age[i] << endl;
+    }
+
+
+    return 0;
+}
