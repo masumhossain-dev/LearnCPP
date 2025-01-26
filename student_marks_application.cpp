b967960651e7526bd1bd66e2b30bbf5fd813b224
masumhossain-dev <masumhossain.dev@gmail.com>
2025-01-26 19:05:54 +0600

    calculate the minimum and maximum marks of students

--- student_marks_application.cpp
+++ student_marks_application.cpp
@@ -12,6 +12,7 @@ int n, sum = 0;
     cout << "Enter the students marks: " << endl;
     for (int i = 0; i< n; i++)
     {
+        cout << "Marks for student " << i + 1 << endl;
         cin >> students[i];
         sum = sum + students[i];
     }
@@ -19,6 +20,25 @@ int n, sum = 0;
     cout << "Total students marks: " << sum << endl;
 
     cout << "Average of students marks: " << (float)sum / n << endl;
+
+
+    //get maximum and minimum marks
+    int max = students[0];
+    int min = students[0];
+
+    for (int i = 1; i < n; i++)
+    {
+        if (students[i] > max)
+        {
+            max = students[i];
+        }
+        if (students[i] < min)
+        {
+            min = students[i];
+        }
+    }
+    cout << "Max mark is: " << max << endl;
+    cout << "Min mark is: " << min << endl;
 
 
     return 0;
